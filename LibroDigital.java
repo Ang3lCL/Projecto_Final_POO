3d98a4fe0b97f3a8796fda0ee51a7b1e55abff3d
unknown <cordova.lalo58@gmail.com>
2026-02-27 22:26:42 -0600

    4.1

--- LibroDigital.java
+++ LibroDigital.java
@@ -2,11 +2,10 @@ package projecto_final;
 
 public class LibroDigital extends Libro {
 
-    private String formato;   // Ej: "PDF", "EPUB", "MOBI"
-    private double tamanioMB; // Tamaño del archivo en MB
+    private String formato;  
+    private double tamanioMB; 
 
-    // Constructor: usa super() para inicializar los atributos de Libro
-    // Se pasa 999 como copias porque un libro digital no se agota
+
     public LibroDigital(String titulo, String autor, String genero, int año, String formato, double tamanioMB) {
         super(titulo, autor, genero, año, 999);
         this.formato = formato;
@@ -21,19 +20,19 @@ public class LibroDigital extends Libro {
         return tamanioMB;
     }
 
-    // Un libro digital siempre se puede "prestar" (es un archivo, no se agota)
+
     @Override
     public boolean prestar() {
         return true;
     }
 
-    // La "devolución" también siempre es válida
+
     @Override
     public boolean devolver() {
         return true;
     }
 
-    // Se extiende el toString() de Libro con la info extra del formato digital
+    
     @Override
     public String toString() {
         return super.toString()
